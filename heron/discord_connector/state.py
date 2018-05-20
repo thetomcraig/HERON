bbb58d9265b8dbbff738a38505381f465c0ee6c9
Tom Craig <thetomcraig@icloud.com>
2018-05-20 16:44:45 -0700

    Debugging the alternation of speakers

--- heron/discord_connector/state.py
+++ heron/discord_connector/state.py
@@ -1,13 +1,15 @@
 messages = []
 next_speaker = ''
 next_message = ''
-bots_in_group_convo = set(['angry_bot', 'joy_bot', 'sad_bot'])
+
+bots_in_group_convo = ['angry_bot', 'trump_bot']
+
 bot_ids_to_names = {
-    'NDEyMzAzNDM2OTk1MTAwNjgy.DWIYng._QQ4-O3teZmuO42S92bDLiYi6mg': 'angry_bot',
-    'NDEyMzA2MDg1Mjk1MjkyNDE4.DWIYvg.ZmgLGCkonDoc1Nu6q-WqkcxnROw': 'joy_bot',
-    '412303436995100682': 'Tom',
-    'fake': 'sad_bot',
+    '412303436995100682': 'trump_bot',
+    '412306085295292418': 'angry_bot',
 }
 bot_names_to_ids = {}
 for k, v in bot_ids_to_names.iteritems():
     bot_names_to_ids[v] = k
+
+BLACK_LIST = ['sad_bot', 'thetomcraig']
--- heron/discord_connector/views.py
+++ heron/discord_connector/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import copy
 import json
 from . import state
 from bots.helpers.twitter_bot_utils import add_message_to_group_convo
@@ -15,21 +16,20 @@ from django.views.decorators.csrf import csrf_exempt
 def message_received(request):
     print('received call')
     body = json.loads(request.body.decode('utf-8'))
-    key = body.get('key')
+    username = body.get('name')
     message = body.get('message')
     conversation_name = body.get('conversation_name')
-    username = state.bot_ids_to_names[key]
 
     # First save the new message to the database
     add_message_to_group_convo(username, message, conversation_name)
 
-    next_speaker, next_message = generate_next_speaker_and_message(conversation_name)
+    repliers_key, next_message = generate_next_speaker_and_message(conversation_name)
 
-    state.speaker = next_speaker
     state.message = next_message
-    return JsonResponse({'success': 'true',
-                         'speaker': state.speaker,
-                         'message': state.message})
+
+    reply = state.message
+    return JsonResponse({'key': repliers_key,
+                         'reply': reply})
 
 
 def reply_query(request):
@@ -37,28 +37,33 @@ def reply_query(request):
 
 
 def generate_next_speaker_and_message(conversation_name):
+    next_speaker_id = generate_next_speaker(conversation_name)
+    next_message = 'bot reply'
+    return next_speaker_id, next_message
+
+
+def generate_next_speaker(conversation_name):
     """
     Given the conversatino name
     Get the conversation and all previous posts - there should be at least one
     """
     convo = TwitterConversation.objects.get(name=conversation_name)
     posts = convo.twitterconversationpost_set.order_by('index').all()
-    next_speaker = posts.last().author.username
-    next_message = 'yup'
+    last_speaker = posts.last().author.username
+    print('last_speaker')
+    print(last_speaker)
+    possible_next_speakers = copy.deepcopy(state.bots_in_group_convo)
+    print('possible_next_speakers')
+    print(possible_next_speakers)
+    try:
+        possible_next_speakers.remove(last_speaker)
+    except:
+        pass
+    next_speaker = possible_next_speakers[0]
 
-    # If only one bot has spoken so far
-    # Randomly choose a new one from the list of bots,
-    # minus the one in question
-    # If two have spoken, possibly choose a new one
-    unique_speakers_so_far = set([x.author.username for x in posts])
-    number_unique_speakers_so_far = len(unique_speakers_so_far)
-    if number_unique_speakers_so_far == 1:
-        next_speaker = random.choice(list(state.bots_in_group_convo - unique_speakers_so_far))
-    if number_unique_speakers_so_far == 2:
-        if random.randint(1, 2) == 1:
-            next_speaker = random.choice(list(state.bots_in_group_convo - unique_speakers_so_far))
-        else:
-            next_speaker = posts[-2].author.username
 
+    print('next_speaker')
+    print(next_speaker)
+    state.speaker = next_speaker
     next_speaker_id = state.bot_names_to_ids[next_speaker]
-    return next_speaker_id, next_message
+    return next_speaker_id
