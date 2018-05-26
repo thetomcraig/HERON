1fc3d8317309c30088d70e8fac4ebad53b2bfc9e
Tom Craig <thetomcraig@icloud.com>
2018-05-26 14:15:09 -0700

    Rework the genration logic

--- heron/discord_connector/views.py
+++ heron/discord_connector/views.py
@@ -7,6 +7,7 @@ from .state_manager import initialize_conversation_sate, add_bot_to_conversation
 from bots.helpers.twitter_bot_utils import add_message_to_group_convo
 from bots.models.twitter import TwitterConversation, TwitterBot, TwitterPost
 import random
+from time import sleep
 
 from django.http import JsonResponse
 
@@ -32,28 +33,56 @@ def bot_online(request):
 def start_generator(_):
     conversation_name = 'general'
     state = settings.DISCORD_CONVERSATION_STATES.get(conversation_name, {})
-    print (state)
+
+    generate = True
+    delay = 5
+
+    while (generate):
+        sleep(delay)
+        state = settings.DISCORD_CONVERSATION_STATES.get(conversation_name, {})
+        print (state)
+        print ('\n')
+        next_speaker_id, next_message = generate_next_speaker_and_message(state, conversation_name)
+        print(next_speaker_id)
+        print(next_message)
+        print ('\n')
+        print ('\n')
 
     return JsonResponse({'success': True})
 
 
-def generate_next_speaker_and_message(conversation_name):
-    next_speaker_id = generate_next_speaker(conversation_name)
+def generate_next_speaker_and_message(state, conversation_name):
+    convo = TwitterConversation.objects.get(name=conversation_name)
+    posts = convo.twitterconversationpost_set.order_by('index').all()
+    last_speaker = posts.last().author.username
+    last_index = posts.last().index
+    index = last_index+1
+
+    next_speaker = generate_next_speaker(state, last_speaker)
+
     next_message = 'bot reply'
+
+    bot = TwitterBot.objects.get(username=next_speaker)
+    post = TwitterPost.objects.create(author=bot, content=next_message)
+    convo.twitterconversationpost_set.create(index=index, author=bot)
+
+    order_by('index').all()
+
     return next_speaker_id, next_message
 
 
-def generate_next_speaker(conversation_name):
+def generate_next_speaker(state, last_speaker):
     """
     Given the conversatino name
     Get the conversation and all previous posts - there should be at least one
     """
-    convo = TwitterConversation.objects.get(name=conversation_name)
-    posts = convo.twitterconversationpost_set.order_by('index').all()
-    last_speaker = posts.last().author.username
     print('last_speaker')
     print(last_speaker)
-    possible_next_speakers = copy.deepcopy(state.bots_in_group_convo)
+    possible_next_speakers = copy.deepcopy(state.get('bots_in_group_convo'))
+    if len(possible_next_speakers) < 2:
+        print('not enough people conversation yet')
+        return -1
+
     print('possible_next_speakers')
     print(possible_next_speakers)
     try:
@@ -64,6 +93,4 @@ def generate_next_speaker(conversation_name):
 
     print('next_speaker')
     print(next_speaker)
-    state.speaker = next_speaker
-    next_speaker_id = state.bot_names_to_ids[next_speaker]
-    return next_speaker_id
+    return next_speaker
