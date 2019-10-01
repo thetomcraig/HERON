86798d2457bb7687670f7cec6b7c144f29471ed2
Tom Craig <thetomcraig@icloud.com>
2019-09-30 21:50:56 -0700

    Lint file

--- heron/bots/helpers/twitter_api_interface.py
+++ heron/bots/helpers/twitter_api_interface.py
@@ -10,74 +10,76 @@ from bs4 import BeautifulSoup
 
 class TwitterApiInterface:
 
-    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
-        """
-        Setup, using credentials from Twitter
-        """
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
+  def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+    """
+    Setup, using credentials from Twitter
+    """
+    self.consumer_key = consumer_key
+    self.consumer_secret = consumer_secret
+    self.access_token = access_token
+    self.access_token_secret = access_token_secret
 
-        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        self.auth.set_access_token(self.access_token, self.access_token_secret)
+    self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+    self.auth.set_access_token(self.access_token, self.access_token_secret)
 
-        self.api = tweepy.API(self.auth)
+    self.api = tweepy.API(self.auth)
 
+  def scrape_top_users(self, limit):
     # Top 100 users
-    def scrape_top_users(self, limit):
-        people_dict = []
-        browser = mechanize.Browser()
-        ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0 (compatible;)'
-        browser.addheaders = [('User-Agent', ua), ('Accept', '*/*')]
+    people_dict = []
+    browser = mechanize.Browser()
+    ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0 (compatible;)'
+    browser.addheaders = [('User-Agent', ua), ('Accept', '*/*')]
+    browser.set_handle_robots(False)
 
-        url = 'http://twittercounter.com/pages/100'
-        browser.open(url)
+    url = 'http://twittercounter.com/pages/100'
+    browser.open(url)
 
-        html = browser.response().read().decode('utf-8', 'ignore')
-        raw = BeautifulSoup(html, "html.parser")
+    html = browser.response().read().decode('utf-8', 'ignore')
+    raw = BeautifulSoup(html, "html.parser")
+    browser.close(url)
 
-        def has_class_and_data_pos(tag):
-            return tag.has_attr('class') and tag.has_attr('data-pos')
-        people = raw.find_all(has_class_and_data_pos)
-        print raw
-        for person in people:
-            name = person.find(class_='name').text
-            uname = person.find(class_='uname').text.replace('@', '')
-            avatar = person.find('img')['src']
-            people_dict.append({'name': name, 'uname': uname, 'avatar': avatar})
+    def has_class_and_data_pos(tag):
+      return tag.has_attr('class') and tag.has_attr('data-pos')
+    people = raw.find_all(has_class_and_data_pos)
+    print raw
+    for person in people:
+      name = person.find(class_='name').text
+      uname = person.find(class_='uname').text.replace('@', '')
+      avatar = person.find('img')['src']
+      people_dict.append({'name': name, 'uname': uname, 'avatar': avatar})
 
-            if len(people_dict) >= limit:
-                return people_dict
+      if len(people_dict) >= limit:
         return people_dict
+    return people_dict
 
-    def get_tweets_from_user(self, username, limit):
-        alltweets = self.api.user_timeline(screen_name=username, count=limit)
-        full_length_tweets = {}
-        for tweet in alltweets:
-            full_length_tweet = self.api.get_status(tweet.id, tweet_mode='extended')._json['full_text']
-            full_length_tweets[tweet.id] = full_length_tweet.encode("utf-8")
+  def get_tweets_from_user(self, username, limit):
+    alltweets = self.api.user_timeline(screen_name=username, count=limit)
+    full_length_tweets = {}
+    for tweet in alltweets:
+      full_length_tweet = self.api.get_status(tweet.id, tweet_mode='extended')._json['full_text']
+      full_length_tweets[tweet.id] = full_length_tweet.encode("utf-8")
 
-        return full_length_tweets
+    return full_length_tweets
 
-    def findTrends(self):
-        # This will be a one element dict
-        trends = self.api.trends_place(1)
-        # Extract the actual trends
-        data = trends[0]
-        # These are all JSON objects
-        # with a name element
-        parsedTrends = data['trends']
-        # Taking out just the names
-        names = [trend['name'] for trend in parsedTrends]
-        # Finally, fixing the encoding
-        for i in range(len(names)):
-            names[i] = names[i].encode('ascii', 'ignore')
-        return names
+  def findTrends(self):
+    # This will be a one element dict
+    trends = self.api.trends_place(1)
+    # Extract the actual trends
+    data = trends[0]
+    # These are all JSON objects
+    # with a name element
+    parsedTrends = data['trends']
+    # Taking out just the names
+    names = [trend['name'] for trend in parsedTrends]
+    # Finally, fixing the encoding
+    for i in range(len(names)):
+      names[i] = names[i].encode('ascii', 'ignore')
+    return names
 
-    # Searches for tweets that use a given tag
-    def search(self, tag):
-        results = self.api.search(q=tag)
-        for i in range(len(results)):
-            results[i] = (results[i]).text.encode('ascii', 'ignore')
-        return results
+  # Searches for tweets that use a given tag
+  def search(self, tag):
+    results = self.api.search(q=tag)
+    for i in range(len(results)):
+      results[i] = (results[i]).text.encode('ascii', 'ignore')
+    return results
