427b9a5affa7e19bfba2b5d060fc6cd971e58fe1
Tom Craig <thetomcraig@icloud.com>
2020-01-22 16:26:21 -0800

    Add logic to strip @ symbol

--- heron/bots/helpers/twitter_bot_utils/scraping_utils.py
+++ heron/bots/helpers/twitter_bot_utils/scraping_utils.py
@@ -71,8 +71,16 @@ def scrape_all_twitter_bots():
 
 
 def scrape(username):
+  if not username:
+    return {'success': False}
+
+  # If the @ symbol was included, strip it
+  if username[0] == '@':
+    username = username[1:]
+
   bot = TwitterBot.objects.get(username=username)
   scrape_response = scrape_twitter_bot(bot)
-  data = {'success': True, 'new tweets': scrape_response['new_tweets'], 'num_new_tweets':
-          scrape_response['num_new_tweets']}
+  data = {'success': True,
+          'new tweets': scrape_response['new_tweets'],
+          'num_new_tweets': scrape_response['num_new_tweets']}
   return data
